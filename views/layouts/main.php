ca7ec7585da666fa622b7a3158d4286e40415e40
Puththai kert <keoutpothe2014@gmail.com>
2021-10-24 21:17:47 +0700

    saving

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -11,7 +11,7 @@
     <title><?= $view_bag['title']?></title>
   </head>
   <body>
-      <div class="containe-fluid bg-secondary">
+      <div class="containe-fluid bg-primary">
         <section class="container">
           <nav class="navbar navbar-expand-lg navbar-dark ">
                 <a href="" class="navbar-brand">Tingdog</a>
